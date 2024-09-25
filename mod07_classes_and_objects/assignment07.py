eecf885046fee630cd79fef26d82e8efa9c8d946
Andrea Hilgendorf <andrea@hilgendorf.me>
2024-09-24 20:23:23 -0700

    added module 07

---
+++ mod07_classes_and_objects/assignment07.py
@@ -0,0 +1,303 @@
+# ------------------------------------------------------------------------------------------ #
+# Title: Assignment07
+# Desc: This assignment demonstrates using data classes
+#   with structured error handling
+# Change Log: (Who, When, What)
+#   AHilgendorf,9/24/2024,Created Script
+# ------------------------------------------------------------------------------------------ #
+import json
+
+class Person:
+    '''
+    Represents a person with first name and last name as its basic attributes
+
+    ChangeLog: (Who, When, What)
+    AHilgendorf,9/24/2024,Created Class
+    '''
+    def __init__(self, first_name: str, last_name: str):
+        self._first_name = first_name
+        self._last_name = last_name
+    
+    @property
+    def first_name(self) -> str:
+        return self._first_name
+    
+    @first_name.setter
+    def first_name(self, value:str):
+        if value.isalpha:
+            self._first_name = value
+        else:
+            raise ValueError("The first name should be letters only.")
+
+    @property
+    def last_name(self) -> str:
+        return self._last_name
+    
+    @last_name.setter
+    def last_name(self, value:str):
+        if value.isalpha:
+            self._last_name = value
+        else:
+            raise ValueError("The last name should be letters only.")
+        
+    def __str__(self):
+        return(f"{self.first_name}, {self.last_name}")
+
+        
+class Student(Person):
+    '''
+    Inherits a student from the class Person adding the course attribute
+
+    ChangeLog: (Who, When, What)
+    AHilgendorf,9/24/2024,Created Class
+    '''
+    def __init__(self, first_name: str, last_name: str, course_name: str):
+        super().__init__(first_name=first_name, last_name=last_name)
+        self._course_name = course_name
+
+    @property
+    def course_name(self) -> str:
+        return self._course_name
+        
+    @course_name.setter
+    def course_name(self, value:str):
+        if value.isalnum:
+            self._course_name = value
+        else:
+            raise ValueError("The course name should be alphanumeric and contain only valid input.")
+    
+    def __str__(self):
+        result = super().__str__()
+        return(f"{result}, {self.course_name}")
+
+# Processing --------------------------------------- #
+
+class FileProcessor:
+    '''
+    A collection of processing layer functions that work with Json files
+
+    ChangeLog: (Who, When, What)
+    AHilgendorf,9/24/2024,Created Class
+    '''
+    @staticmethod
+    def read_data_from_file(file_name: str, student_data: list) -> list[Student]:
+        """ This function reads data from a json file and loads it into the program
+
+        ChangeLog: (Who, When, What)
+        AHilgendorf,9/23/2024,Created function
+
+        :param file_name: string data with name of file to read from
+        :param student_data: list of dictionary rows to be filled with file data
+
+        :return: list
+        """
+        student: Student
+        try:
+            students = []
+            file = open(file_name, "r")
+            student_data = json.load(file)
+            for student in student_data:
+                students.append(Student(first_name=student['FirstName'], last_name=student['LastName'], course_name=student['CourseName']))
+
+            file.close()
+        except Exception as e:
+            IO.output_error_messages(message="Error: There was a problem with reading the file.", error=e)
+
+        finally:
+            if file.closed == False:
+                file.close()
+        return students
+
+    @staticmethod
+    def write_data_to_file(file_name: str, student_data: list):
+        """ This function writes data to a json file
+
+        ChangeLog: (Who, When, What)
+        AHilgendorf,9/24/2024,Created function
+
+        :param file_name: string data with name of file to write to
+        :param student_data: list of dictionary rows to be writen to the file
+
+        :return: None
+        """
+        student_list: list = []
+        for student in student_data:
+            student_list.append({"FirstName": student.first_name, "LastName": student.last_name, "CourseName": student.course_name})
+
+        print("test student json: ")
+        print(student_list)
+        try:
+            file = open(file_name, "w")
+            json.dump(student_list, file)
+            file.close()
+            IO.output_student_and_course_names(student_data=student_data)
+        except Exception as e:
+            print(e)
+            message = "Error: There was a problem with writing to the file.\n"
+            message += "Please check that the file is not open by another program."
+            IO.output_error_messages(message=message,error=e)
+        finally:
+            if file.closed == False:
+                file.close()
+
+
+# Presentation --------------------------------------- #
+
+class IO:
+    '''
+    A collection of presentation layer functions that manage user input and output
+
+    ChangeLog: (Who, When, What)
+    AHilgendorf,9/24/2024,Created Class
+    '''
+
+    @staticmethod
+    def output_error_messages(message: str, error: Exception = None):
+        """ This function displays a custom error messages to the user
+
+        ChangeLog: (Who, When, What)
+        AHilgendorf,9/24/2024,Created function
+
+        :param message: string with message data to display
+        :param error: Exception object with technical message to display
+
+        :return: None
+        """
+        print(message, end="\n\n")
+        if error is not None:
+            print("-- Technical Error Message -- ")
+            print(error, error.__doc__, type(error), sep='\n')
+
+    @staticmethod
+    def output_menu(menu: str):
+        """ This function displays the menu of choices to the user
+
+        ChangeLog: (Who, When, What)
+        AHilgendorf,9/24/2024,Created function
+
+
+        :return: None
+        """
+        print()  # Adding extra space to make it look nicer.
+        print(menu)
+        print()  # Adding extra space to make it look nicer.
+
+    @staticmethod
+    def input_menu_choice():
+        """ This function gets a menu choice from the user
+
+        ChangeLog: (Who, When, What)
+        AHilgendorf,9/24/2024,Created function
+
+        :return: string with the users choice
+        """
+        choice = "0"
+        try:
+            choice = input("Enter your menu choice number: ")
+            if choice not in ("1","2","3","4"):  # Note these are strings
+                raise Exception("Please, choose only 1, 2, 3, or 4")
+        except Exception as e:
+            IO.output_error_messages(e.__str__())  # Not passing e to avoid the technical message
+
+        return choice
+
+    @staticmethod
+    def output_student_and_course_names(student_data: list[Student]):
+        """ This function displays the student and course names to the user
+
+        ChangeLog: (Who, When, What)
+        AHilgendorf,9/24/2024,Created function
+
+        :param student_data: list of dictionary rows to be displayed
+
+        :return: None
+        """
+        student_first_name: str
+        student_last_name: str
+        student_course_name: str
+        print("-" * 50)
+        for student in student_data:
+            print(f'Student {student.first_name} '
+                  f'{student.last_name} is enrolled in {student.course_name}')
+        print("-" * 50)
+
+    @staticmethod
+    def input_student_data(student_data: list[Student]) -> list[Student]:
+        """ This function gets the student's first name and last name, with a course name from the user
+
+        ChangeLog: (Who, When, What)
+        AHilgendorf,9/24/2024,Created function
+
+        :param student_data: list of dictionary rows to be filled with input data
+
+        :return: list
+        """
+
+        try:
+            student_first_name = input("Enter the student's first name: ")
+            student_last_name = input("Enter the student's last name: ")
+            if not student_last_name.isalpha():
+                raise ValueError("The last name should not contain numbers.")
+            course_name = input("Please enter the name of the course: ")
+            student = Student(first_name=student_first_name, 
+                last_name=student_last_name, 
+                course_name=course_name
+            )
+
+            student_data.append(student)
+
+            print(f"\nYou have registered {student_first_name} {student_last_name} for {course_name}.")
+        except ValueError as e:
+            IO.output_error_messages(message="One of the values was the correct type of data!", error=e)
+        except Exception as e:
+            IO.output_error_messages(message="Error: There was a problem with your entered data.", error=e)
+        return student_data
+
+MENU: str = '''
+---- Course Registration Program ----
+  Select from the following menu:  
+    1. Register a Student for a Course.
+    2. Show current data.  
+    3. Save data to a file.
+    4. Exit the program.
+----------------------------------------- 
+'''
+FILE_NAME: str = "Enrollments.json"
+
+# Define the Data Variables
+menu_choice: str = []
+
+students: list[Student] = []  # a table of student data
+menu_choice: str  # Hold the choice made by the user.
+
+students: list[Student] = FileProcessor.read_data_from_file(file_name=FILE_NAME, student_data=students)
+
+# Present and Process the data
+while (True):
+    # Present the menu of choices
+    IO.output_menu(menu=MENU)
+
+    menu_choice = IO.input_menu_choice()
+
+    # Input user data
+    if menu_choice == "1":
+        students = IO.input_student_data(student_data=students)
+        continue
+
+    # Present the current data
+    elif menu_choice == "2":
+        IO.output_student_and_course_names(students)
+        continue
+
+    # Save the data to a file
+    elif menu_choice == "3":
+        FileProcessor.write_data_to_file(file_name=FILE_NAME, student_data=students)
+        continue
+
+    # Stop the loop
+    elif menu_choice == "4":
+        break  # out of the loop
+    else:
+        print("Please only choose option 1, 2, or 3")
+
+print("Program Ended")
---
+++ mod07_classes_and_objects/assingment07_andrea_hilgendorf.md
@@ -0,0 +1,150 @@
+﻿Andrea Hilgendorf September 24th, 2024 IT FDN 110 B Assignment 07 
+
+Classes and Objects
+
+Introduction 
+
+In module 07 I learned about the di erences between statements, functions, and classes. These three elements are the basic component of a program. I worked with classes, constructors, attributes, and properties. I also learned about the di erence between data classes, presentation classes, and processing classes. Finally,  I also learned about Git uses and functions like the .GitIgnore and sharing my projects in GitHub with commit and push.  
+
+Steps 
+
+1. File naming convention: to comply with the assignment criteria, I named my Python file “assignment07.py”. 
+
+   ![](Aspose.Words.e0b95d9c-d6b6-434c-aa29-30892c0541a5.001.png)
+
+   *Figure 1: File Name* 
+
+2. Script Header:  I typed a header for my script, which I included the necessary ‘#’ and ‘-’ symbols, plus an appropriate title and description, and a change log to include my name as the author, today’s date, and the type of work I did in this file. 
+
+   ![](Aspose.Words.e0b95d9c-d6b6-434c-aa29-30892c0541a5.002.png)
+
+   *Figure 2: Script Header* 
+
+3. Using an import statement to access a library to use the JSON functionalities.  
+
+   ![](Aspose.Words.e0b95d9c-d6b6-434c-aa29-30892c0541a5.003.png)
+
+   *Figure 3: import statement* 
+
+4. Defining constants and variables: I followed the instructions to set up two constants (MENU and FILE\_NAME) and two variables (menu\_choice and students) for the program. Each constant and variable is accompanied by its data type and then set up with a value or as an empty string or an empty list. There are additional variables declared as needed.  
+
+   ![](Aspose.Words.e0b95d9c-d6b6-434c-aa29-30892c0541a5.004.jpeg)
+
+   *Figure 4: Constants and Variables* 
+
+5. Classes: I included four classes, FileProcessor, IO, Person, and Student. Each includes a descriptive documenting string: 
+
+   ![](Aspose.Words.e0b95d9c-d6b6-434c-aa29-30892c0541a5.005.png)
+
+   *Figure 5: class with descriptive string* 
+
+   ![](Aspose.Words.e0b95d9c-d6b6-434c-aa29-30892c0541a5.006.png)
+
+   *Figure 6: class with descriptive string* 
+
+   ![](Aspose.Words.e0b95d9c-d6b6-434c-aa29-30892c0541a5.007.png)
+
+   *Figure 7: class with descriptive string* 
+
+   ![](Aspose.Words.e0b95d9c-d6b6-434c-aa29-30892c0541a5.008.png)
+
+   *Figure 8: class with descriptive string* 
+
+6. Class Properties: my program includes three properties for the student first name, student last name, and the course name.s 
+
+   ![](Aspose.Words.e0b95d9c-d6b6-434c-aa29-30892c0541a5.009.jpeg)
+
+   *Figure 9: Class properties with validation code set to empty strings* 
+
+   ![](Aspose.Words.e0b95d9c-d6b6-434c-aa29-30892c0541a5.010.png)
+
+   *Figure 10: Class property with simple validation code set to an empty string* 
+
+7. Class Methods: my program includes two string methods for extracting data from the Person and Student classes: 
+
+   ![](Aspose.Words.e0b95d9c-d6b6-434c-aa29-30892c0541a5.011.png)
+
+   *Figure 11: class method to extract data from the class Person* 
+
+   ![](Aspose.Words.e0b95d9c-d6b6-434c-aa29-30892c0541a5.012.png)
+
+   *Figure 12: class method to extract data from the class Student* 
+
+8. Functions: I defined eight di erent functions each with a descriptive string, except blocks that call out the error handling message. All eight functions use the @staticmethod decorator. 
+1. Method 1: output\_error\_messages(message: str, error: Exception = None) 
+
+   ![](Aspose.Words.e0b95d9c-d6b6-434c-aa29-30892c0541a5.013.jpeg)
+
+   *Figure 13: Method 1 with descriptive string – within IO class* 
+
+2. Method 2: output\_menu(menu: str) 
+
+   ![](Aspose.Words.e0b95d9c-d6b6-434c-aa29-30892c0541a5.014.png)
+
+   *Figure 14: Method 2 with descriptive string – within IO Class* 
+
+3. Method 3: input\_menu\_choice(): 
+
+   ![](Aspose.Words.e0b95d9c-d6b6-434c-aa29-30892c0541a5.015.jpeg)
+
+*Figure 15: Method 3 with descriptive string – within IO class* 
+
+4. Method 4: output\_student\_courses(student\_data: list) 
+
+   ![](Aspose.Words.e0b95d9c-d6b6-434c-aa29-30892c0541a5.016.jpeg)
+
+   *Figure 16: Method 4 with descriptive string –  within IO Class* 
+
+5. Method 5: input\_student\_data(student\_data: list)
+
+   ![](Aspose.Words.e0b95d9c-d6b6-434c-aa29-30892c0541a5.017.jpeg)
+
+*Figure 17: Method 5 with descriptive string – within IO Class* 
+
+6. Method 6: read\_data\_from\_file(file\_name: str, student\_data: list):
+
+   ![](Aspose.Words.e0b95d9c-d6b6-434c-aa29-30892c0541a5.018.jpeg)
+
+   *Figure 18: Method 6 with descriptive string – within the FileProcessor Class* 
+
+7. Method 7: write\_data\_to\_file (file\_name: str, student\_data: list):
+
+   ![](Aspose.Words.e0b95d9c-d6b6-434c-aa29-30892c0541a5.019.jpeg)
+
+   *Figure 19: Method 7 with descriptive string – within the FileProcessor Class* 
+
+9. Processing data and collecting Input/Output: Each of the menu choices are presented to the user and the user’s input is collected. On menu choice 1, the user can enter student data. On menu choice 2, the user can see the collected data. On menu choice 3, the program writes data to the .json file. Lastly, menu choice 4 breaks the loop and the program ends:  
+
+
+![](Aspose.Words.e0b95d9c-d6b6-434c-aa29-30892c0541a5.020.jpeg)
+
+*Figure 20: Program’s input/output and processing of data.*  
+
+10. Error handling is included for handling errors while reading the file, for user’s input, and for problems while trying to read the .json file. 
+
+    ![](Aspose.Words.e0b95d9c-d6b6-434c-aa29-30892c0541a5.021.png)
+
+    *Figure 21: error handling* 
+
+    ![](Aspose.Words.e0b95d9c-d6b6-434c-aa29-30892c0541a5.022.png)
+
+    *Figure 22: error handling* 
+
+Summary 
+
+This assignment demonstrated my understanding of methods and classes, as well as using GitHub as a way for source control. I was able to practice using a JSON file, writing a header, defining constants and variables, adding comments to my script, opening and closing files, presenting and processing data, saving the data to a list, and then to a file – All while thinking about error handling. Additionally, I was able to run my code both in PyCharm and in my Mac’s terminal. Additionally, I got to build upon my knowledge of Python with the insertion of classes and methods for this module.  
+
+While running my program, I was able to test and see that the program takes in three forms of user input (student’s first and last name, and a course name). The program is also capable of displaying the user’s input collected and saving it to the .json file. The user can enter multiple students to the program and similarly, the program can display multiple registrations and save them to the file.  
+
+The program runs in the Terminal:  
+
+![](Aspose.Words.e0b95d9c-d6b6-434c-aa29-30892c0541a5.023.jpeg)
+
+*Figure23: Running my code in the Terminal* 
+
+GitHub Repository 
+
+- [Link to my GitHub profile ](https://github.com/andreahilgendorf)
+- [Link to my repository which includes Mod07 ](https://github.com/andreahilgendorf/IntroToProg-Python/tree/main/mod06_functions)
+
+**Please note -** I decided to organize my repository by module – hence, a small deviation from the instruction for this assignment.
